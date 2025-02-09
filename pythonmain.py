27dbd4e3c4dc50ee81cb6c4f3d065b8cea560dd5
Sameem <nsam603@gmail.com>
2025-02-09 12:25:24 +0530

    variables

--- pythonmain.py
+++ pythonmain.py
@@ -29,3 +29,40 @@ f = False
 
 g = None #to mark nothing
 
+
+
+#boolean
+d = 1==1
+print(d)
+
+
+
+#logical operators
+
+j = True or False
+print(j)
+
+k = True or True
+print(k)
+
+l = True and False
+print(l)
+
+m = False and False
+print(m)
+
+
+print("True and False is:", True and False)
+
+print(not(False))
+
+# function to check the type of variable
+print(type(a))
+
+#changing the type using type casting 
+a = 1
+b=float(a)
+print(b)
+print(type(b))
+print(type(float(a)))
+
