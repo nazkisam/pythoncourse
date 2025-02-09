641f074ede63132132472ba92f2a758254b35b31
Sameem <nsam603@gmail.com>
2025-02-09 12:05:13 +0530

    variables

---
+++ pythonmain.py
@@ -0,0 +1,31 @@
+#pyjokes library
+
+import pyjokes
+jokes = pyjokes.get_joke()
+print(jokes)
+
+
+#variables
+#int
+a=1
+b=2
+print(a+b)
+
+#string
+c = 'string'
+print(c)
+
+#floating point number
+
+d = 1.4
+print(d)
+
+
+#boolean
+e = True
+f = False
+
+#None
+
+g = None #to mark nothing
+
